b921716c740f8f48e0894b6fe553f36c6061977d
Conrad Buys <conradbuys11@gmail.com>
2020-07-21 17:49:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,17 @@
 # code your #valid_move? method here
-
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def valid_move?(input)
-  
+
+
+def valid_move?(board_arr, input_int)
+  if(position_taken?(board_arr, input_int))
+    
+end
+
+def position_taken?(board_arr, input_int)
+  if(board_arr[input_int] == " " || board_arr[input_int] == "" || board_arr[input_int] == nil)
+    true
+  else
+    false
+  end
 end
