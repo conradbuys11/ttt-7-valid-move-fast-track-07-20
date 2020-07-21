8e190b047721a8b7c355a447bd08b0809547770a
Conrad Buys <conradbuys11@gmail.com>
2020-07-21 18:00:07 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,22 @@
 # code your #valid_move? method here
-
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+
+
+def valid_move?(board_arr, input_int)
+  if board_arr.class == Array && input_int.class == Fixnum
+    if input_int >= 0 && input_int < 9
+      position_taken?(board_arr, input_int)
+    else
+      puts "Please type a number between 1 and 9!"
+    end
+  end
+end
+
+def position_taken?(board_arr, input_int)
+  if(board_arr[input_int] == " " || board_arr[input_int] == "" || board_arr[input_int] == nil)
+    true
+  else
+    false
+  end
+end
